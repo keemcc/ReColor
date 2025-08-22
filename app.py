789d12ba658125d2d2a12c6e9f7813b44e988808
keemcc <ktsmccann@gmail.com>
2025-08-22 01:11:56 -0500

    added functionality to the converter

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ def getDistance(realColor, palletColor):
     palletR, palletG, palletB = palletColor
     return ((realR - palletR)**2 + (realB - palletB)**2 + (realG - palletG)**2)
 
+# Get the closest color match
+def getClosestMatch(realColor, palletColors):
+    closestMatch = None
+    matchDistance = None
+    for palletColor in palletColors:
+        currentDistance = getDistance(realColor, palletColor)
+        if (not closestMatch) or (currentDistance < matchDistance):
+            closestMatch = palletColor
+            matchDistance = currentDistance
+    return closestMatch
+
+
 # Get the pallet colors as a set
 def getPallet(pallet):
     palletColors = set()
@@ -16,21 +28,20 @@ def getPallet(pallet):
             palletColors.add(pixels[x, y])
     return palletColors
 
-original = Image.open("./media/Testx16.png").convert("RGB")
-pixels = original.load()
-width, height = original.size
-
+originalImage = Image.open("./media/Testx16.png").convert("RGB")
 palletImage = Image.open("./media/pallet.png").convert("RGB")
+originalPixels = originalImage.load()
+width, height = originalImage.size
+palletColors = getPallet(palletImage)
 
+colorMap = dict()
 
-colors = getPallet(original)
+originalPixels = originalImage.load()
+for x in range(width):
+    for y in range(height):
+        pixel = originalPixels[x, y]
+        if pixel not in colorMap:
+            colorMap[pixel] = getClosestMatch(pixel, palletColors)
+        originalPixels[x, y] = colorMap[pixel]
 
-
-# #Set grayscale
-# for x in range(width):
-#     for y in range(height):
-#         r, g, b = pixels[x, y]
-#         gray = int((r + g + b) / 3)
-#         pixels[x, y] = (gray, gray, gray)
-
-# original.save("./media/edited.png")
+originalImage.save("./media/edited.png")
