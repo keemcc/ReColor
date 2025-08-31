bd8e1c576e44f346f887e75329e59319b830cebd
keemcc <ktmmg4@umsystem.edu>
2025-08-31 02:05:51 -0500

    made export funciton work

--- recolor.py
+++ recolor.py
@@ -1,5 +1,6 @@
 import argparse, pickle, os
 from functions import safeOpenImage, getPalette, runColorPicker
+from PIL import Image
 
 DATA_PATH = "./data"
 
@@ -24,25 +25,39 @@ def palette(args):
     print(palette)
     savePalette(palette, "./data")
 
-def save_palette(args):
+def export(args):
     paletteColors = set()
     if args.filepath:
         paletteImage = safeOpenImage(filepath=(args.filepath))
         paletteColors = getPalette(paletteImage)
     else:
         paletteColors = loadPalette(DATA_PATH)
+    exportedPalette = Image.new("RGB", (len(paletteColors), 1), (255, 255, 255))
+    exportedPalettePixels = exportedPalette.load()
+    for x in range(len(paletteColors)):
+        exportedPalettePixels[x, 0] = paletteColors.pop()
+    exportedPalette.save("exported_pallet.png")
     
+
 
 def main():
     parser = argparse.ArgumentParser(prog="recolor", description="tools for recoloring images into rgb palettes")
     subparser = parser.add_subparsers()
-    parser_palette = subparser.add_parser('palette')
-    parser_palette.add_argument("-f", "--filepath", help="specify file to use for creating palette")
-    parser_palette.set_defaults(function=palette)
-    parser_savepalette = subparser.add_parser("save-palette")
-    parser_savepalette.add_argument("-f", "--filepath", help="specify file to create and save palette from")
+
+    palette_parser = subparser.add_parser('palette')
+    palette_parser.add_argument("-f", "--filepath", help="specify file to use for creating palette")
+    palette_parser.set_defaults(function=palette)
+    palette_subparser = palette_parser.add_subparsers(dest="palette_command")
+
+    export_parser = palette_subparser.add_parser("export")
+    export_parser.add_argument("-n", "--name", help="specify result name")
+    export_parser.set_defaults(function=export)
+
     args = parser.parse_args()
-    args.function(args)
+    if hasattr(args, "function"):
+        args.function(args)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
