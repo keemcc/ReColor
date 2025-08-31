426832a47d24b3f075703fcc0ca60db43df6745a
keemcc <ktmmg4@umsystem.edu>
2025-08-31 02:23:19 -0500

    added convert command

--- recolor.py
+++ recolor.py
@@ -1,5 +1,5 @@
 import argparse, pickle, os
-from functions import safeOpenImage, getPalette, runColorPicker
+from functions import safeOpenImage, getPalette, runColorPicker, getClosestMatch
 from PIL import Image
 
 DATA_PATH = "./data"
@@ -37,6 +37,24 @@ def export(args):
     for x in range(len(paletteColors)):
         exportedPalettePixels[x, 0] = paletteColors.pop()
     exportedPalette.save("exported_pallet.png")
+
+def convert(args):
+    paletteColors = loadPalette(DATA_PATH)
+    originalImage = safeOpenImage(args.filepath)
+    originalPixels = originalImage.load()
+    width, height = originalImage.size
+    colorMap = dict()
+    for x in range(width):
+        for y in range(height):
+            originalColor = originalPixels[x, y]
+            if originalColor not in colorMap:
+                colorMap[originalColor] = getClosestMatch(originalColor, paletteColors)
+            originalPixels[x, y] = colorMap[originalColor]
+    if args.name:
+        originalImage.save(args.name+".png")
+    else:
+        originalImage.save("edited.png")
+
     
 
 
@@ -52,6 +70,11 @@ def main():
     export_parser = palette_subparser.add_parser("export")
     export_parser.add_argument("-n", "--name", help="specify result name")
     export_parser.set_defaults(function=export)
+
+    convert_parser = subparser.add_parser("convert")
+    convert_parser.add_argument("filepath")
+    convert_parser.add_argument("-n", "--name")
+    convert_parser.set_defaults(function=convert)
 
     args = parser.parse_args()
     if hasattr(args, "function"):
