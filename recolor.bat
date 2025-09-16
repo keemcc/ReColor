289b5d17a130aa36fbf8a142e36e6703bf04b9a5
keemcc <ktsmccann@gmail.com>
2025-09-15 21:43:02 -0500

    added comments and more messages to the batch script

--- recolor.bat
+++ recolor.bat
@@ -3,11 +3,13 @@ setlocal
 
 set VENV_DIR=%~dp0.venv
 
+@REM If the venv directory doesn't exist, venv is not installed, create a virtual environment
 if not exist "%VENV_DIR%" (
     echo No virtual environment detected
     echo Creating virtual environment in %VENV_DIR%
     echo This might take a second!
     python -m venv "%VENV_DIR%"
+    @REM If creation fails, python may not be installed or added to path
     if errorlevel 1 (
         echo Failed to create virtual environment. Please make sure python is installed and added to path.
         exit /b 1
@@ -15,6 +17,7 @@ if not exist "%VENV_DIR%" (
     echo Installing packages...
     call "%VENV_DIR%\Scripts\activate.bat"
     pip install -r "%~dp0requirements.txt" -qqq
+    echo Installation complete!
 )
 
 call "%~dp0.venv\Scripts\activate.bat"
